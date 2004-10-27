797b01a71589e174187e3b4e1856e923b17e6f96
vtigtti <devnull@localhost>
2004-10-27 11:10:02 -0700

    Nabeel: indented the file....

--- io/UDPClient.h
+++ io/UDPClient.h
@@ -1,4 +1,3 @@
-// XXX/demmer add copyright
 #ifndef _UDP_CLIENT_H_
 #define _UDP_CLIENT_H_
 
@@ -17,18 +16,6 @@ public:
      * System call wrapper
      */
     virtual int connect(in_addr_t remote_addr, u_int16_t remote_port);
-
-    /**
-     * Try to connect to the remote host, but don't block for more
-     * than timeout milliseconds. If there was an error (either
-     * immediate or delayed), return it in *errp.
-     *
-     * @return 0 on success, IOERROR on error, and IOTIMEOUT on
-     * timeout.
-     */
-    virtual int timeout_connect(in_addr_t remote_attr, u_int16_t remote_port,
-                                int timeout_ms, int* errp = 0);
-    
     
 protected:
     int internal_connect(in_addr_t remote_attr, u_int16_t remote_port);
