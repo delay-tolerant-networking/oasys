1b2133f3952970a82b24c8c765200dc41f4b4c87
bowei <devnull@localhost>
2004-11-10 17:04:58 -0800

    * fixed String.h -> StringUtils.h

--- util/MD5.h
+++ util/MD5.h
@@ -1,70 +1,81 @@
 // XXX/demmer add copyright
-/* MD5.H - header file for MD5C.C
- */
+#ifndef tier_md5_h
+#define tier_md5_h
 
-/* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
-rights reserved.
+#include <sys/types.h>
+#include <string>
+#include "StringUtils.h"
 
-License to copy and use this software is granted provided that it
-is identified as the "RSA Data Security, Inc. MD5 Message-Digest
-Algorithm" in all material mentioning or referencing this software
-or this function.
+extern "C" {
+#define PROTOTYPES 1
+#include "md5.h"
+}
 
-License is also granted to make and use derivative works provided
-that such works are identified as "derived from the RSA Data
-Security, Inc. MD5 Message-Digest Algorithm" in all material
-mentioning or referencing the derived work.
+#define MD5LEN 16
 
-RSA Data Security, Inc. makes no representations concerning either
-the merchantability of this software or the suitability of this
-software for any particular purpose. It is provided "as is"
-without express or implied warranty of any kind.
+class MD5 {
+public:
+    MD5() {
+        init();
+    }
+    ~MD5() {}
 
-These notices must be retained in any copies of any part of this
-documentation and/or software.
- */
+    void init()
+    {
+        MD5Init(&ctx_);
+    }
+    
+    void update(const u_char* data, size_t len)
+    {
+        MD5Update(&ctx_, (u_char*)data, len);
+    }
 
-/* GLOBAL.H - RSAREF types and constants
- */
+    void update(const char* data, size_t len)
+    {
+        MD5Update(&ctx_, (u_char*)data, len);
+    }
 
-/* PROTOTYPES should be set to one if and only if the compiler supports
-  function argument prototyping.
-  The following makes PROTOTYPES default to 0 if it has not already
-  been defined with C compiler flags.
- */
+    void finalize()
+    {
+        MD5Final(digest_, &ctx_);
+    }
+    
+    const u_char* digest()
+    {
+        return digest_;
+    }
 
-#ifndef PROTOTYPES
-#define PROTOTYPES 0
+    static void digest_ascii(std::string* str,
+                             const u_char* digest)
+    {
+        hex2str(str, digest, MD5LEN);
+    }
+
+    static std::string digest_ascii(const u_char* digest)
+    {
+        std::string str;
+        digest_ascii(&str, digest);
+        return str;
+    }
+
+    void digest_ascii(std::string* str)
+    {
+        digest_ascii(str, digest_);
+    }
+
+    std::string digest_ascii()
+    {
+        return digest_ascii(digest_);
+    }
+
+    static void digest_fromascii(const char* str, u_char* digest)
+    {
+        str2hex(str, digest, MD5LEN);
+    }
+
+private:
+    MD5_CTX ctx_;
+    u_char digest_[MD5LEN];
+};
+
 #endif
-
-/* POINTER defines a generic pointer type */
-typedef unsigned char *POINTER;
-
-/* UINT2 defines a two byte word */
-typedef unsigned short int UINT2;
-
-/* UINT4 defines a four byte word */
-typedef unsigned long int UINT4;
-
-/* PROTO_LIST is defined depending on how PROTOTYPES is defined above.
-If using PROTOTYPES, then PROTO_LIST returns the list, otherwise it
-  returns an empty list.
- */
-#if PROTOTYPES
-#define PROTO_LIST(list) list
-#else
-#define PROTO_LIST(list) ()
-#endif
-
-/* MD5 context. */
-typedef struct {
-  UINT4 state[4];                                   /* state (ABCD) */
-  UINT4 count[2];        /* number of bits, modulo 2^64 (lsb first) */
-  unsigned char buffer[64];                         /* input buffer */
-} MD5_CTX;
-
-void MD5Init PROTO_LIST ((MD5_CTX *));
-void MD5Update PROTO_LIST
-  ((MD5_CTX *, unsigned char *, unsigned int));
-void MD5Final PROTO_LIST ((unsigned char [16], MD5_CTX *));
-
