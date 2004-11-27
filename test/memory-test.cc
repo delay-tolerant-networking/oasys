cc06b9ccf9878ddbe35770de609f9c99bf6ad3a0
bowei <devnull@localhost>
2004-11-26 19:21:14 -0800

    * memory test

--- test/memory-test.cc
+++ test/memory-test.cc
@@ -1,4 +1,59 @@
+#include <list>
+
+#include <debug/Log.h>
+#include <memory/Memory.h>
+
+class Foo_1 {};
+std::list<Foo_1*> lFoos;
+
+void
+Alloc_1()
+{
+    lFoos.push_back(new Foo_1());
+}
+
+void
+Alloc_2()
+{
+    lFoos.push_back(new Foo_1());
+}
+
+void Alloc_3()
+{
+    lFoos.push_back(new Foo_1());
+
+    Alloc_1();
+    Alloc_2();
+}
+
+void
+delete_all_foo()
+{
+    while(lFoos.size() > 0)
+    {
+	Foo_1* obj = lFoos.front();
+	delete obj;
+
+	lFoos.pop_front();
+    }
+}
+
 int
 main(int argc, char* argv[])
 {
+    Log::init(1, LOG_DEBUG, "memory-test.debug");
+    DbgMemInfo::init();
+    
+    // Create 11 Foo_1 object in different places
+    Alloc_3();
+    Alloc_3();
+    Alloc_3();
+    Alloc_2();
+    Alloc_1();
+
+    DbgMemInfo::dump();
+
+    // Delete all Foo_1 objects
+    delete_all_foo();
+    DbgMemInfo::dump();
 }
