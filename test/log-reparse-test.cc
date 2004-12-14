588a1d86047fbb57dd7a67fe20a0d31022b38162
demmer <devnull@localhost>
2004-12-14 15:09:39 -0800

    add tests for log reparsing and the spin lock

---
+++ test/log-reparse-test.cc
@@ -0,0 +1,84 @@
+
+#include <unistd.h>
+#include "debug/Log.h"
+#include "io/FileIOClient.h"
+#include "thread/Thread.h"
+#include "thread/Timer.h"
+#include "util/StringBuffer.h"
+
+using namespace oasys;
+
+class LoggerThread : public Thread {
+    virtual void run () {
+        int i;
+        while (1) {
+            if ((i % 1000) == 0) {
+                log_info("/test/1000", "test 1000 (%d)", i / 1000);
+            }
+            
+            log_debug("/test", "test %d", ++i);
+        }
+    }
+};
+
+int
+main(int argc, const char** argv)
+{
+    Log::init(LOG_WARN);
+    Log* log = Log::instance();
+
+    // first create two files, one with the test rule enabled, one without
+    StringBuffer path1("/tmp/log-reparse-test-%s-1", getenv("USER"));
+    StringBuffer path2("/tmp/log-reparse-test-%s-2", getenv("USER"));
+    
+    FileIOClient* f1 = new FileIOClient();
+    FileIOClient* f2 = new FileIOClient();
+
+    f1->logpathf("/log/file1");
+    f2->logpathf("/log/file2");
+
+    f1->open(path1.c_str(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    f2->open(path2.c_str(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    
+    const char* debug1 = "/log info\n/test debug\n/test/1000 info\n";
+    const char* debug2 = "/log info\n/test/1000 info\n";
+    f1->write(debug1, strlen(debug1));
+    f2->write(debug2, strlen(debug2));
+
+    f1->close();
+    f2->close();
+
+    // set up the reparse signal handler
+    TimerSystem::init();
+    log->add_reparse_handler(SIGHUP);
+
+    // start up the thread, nothing should output (yet)
+    LoggerThread* t = new LoggerThread();
+    t->start();
+
+    // parse the first one, now we start getting output
+    log->parse_debug_file(path1.c_str());
+    sleep(1);
+
+    // reparse the same one using a signal
+    kill(getpid(), SIGHUP);
+    sleep(1);
+        
+    // now parse the other one, output should cease
+    log->parse_debug_file(path2.c_str());
+    sleep(1);
+    
+    // reparse the same one using a signal
+    kill(getpid(), SIGHUP);
+    sleep(1);
+
+    // try adding a rule explicitly.. we should get output again
+    log_info("/log", "adding explicit debug rule");
+    log->add_debug_rule("/test", LOG_DEBUG);
+    sleep(1);
+    
+    // all done!
+
+    f1->unlink();
+    f2->unlink();
+}
---
+++ test/spin-lock-test.cc
@@ -0,0 +1,102 @@
+#include <cstdio>
+#include <sys/time.h>
+
+#include "debug/Log.h"
+#include <thread/SpinLock.h>
+#include <thread/Thread.h>
+
+using namespace oasys;
+
+volatile int nspins = 10000000;
+volatile int total  = 0;
+volatile int count1 = 0;
+volatile int count2 = 0;
+
+class Thread1 : public Thread {
+public:
+    Thread1(SpinLock* l) : lock_(l) {}
+    
+protected:
+    virtual void run() {
+        for (int i = 0; i < nspins; ++i) {
+            if (lock_)
+                lock_->lock();
+            ++count1;
+            ++total;
+            if (lock_)
+                lock_->unlock();
+        }
+    }
+
+    SpinLock* lock_;
+};
+
+class Thread2 : public Thread {
+public:
+    Thread2(SpinLock* l) : lock_(l) {}
+    
+protected:
+    virtual void run() {
+        for (int i = 0; i < nspins; ++i) {
+            if (lock_)
+                lock_->lock();
+            
+            ++count2;
+            ++total;
+            if (lock_)
+                lock_->unlock();
+        }
+    }
+
+    SpinLock* lock_;
+};
+
+void
+test(const char* what, SpinLock* lock1, SpinLock* lock2)
+{
+    log_info("/test", "***** testing with %s lock...", what);
+    Thread* t1 = new Thread1(lock1);
+    Thread* t2 = new Thread2(lock2);
+
+    t1->start();
+    t2->start();
+
+    while (count1 != nspins && count2 != nspins) {
+        log_info("/test", "count1:     %d", count1);
+        log_info("/test", "count2:     %d", count2);
+        sleep(1);
+    }
+
+    t1->join();
+    t2->join();
+
+    log_info("/log", "total spins: %d",  SpinLock::total_spins_);
+    log_info("/log", "total yields: %d", SpinLock::total_yields_);
+
+    log_info("/test", "count1:     %d", count1);
+    log_info("/test", "count2:     %d", count2);
+    log_info("/test", "total:      %d", total);
+    log_info("/test", "count sum:  %d", count1 + count2);
+
+    log_info("/test", "***** testing with %s lock... done.\n\n", what);
+    delete t1;
+    delete t2;
+
+    count1 = 0;
+    count2 = 0;
+    total = 0;
+    SpinLock::total_spins_ = 0;
+    SpinLock::total_yields_ = 0;
+}
+
+int
+main()
+{
+    Log::init(LOG_DEBUG);
+    SpinLock lock;
+    SpinLock lock2;
+
+    test("shared",   &lock, &lock);
+    test("separate", &lock, &lock2);
+    test("no", 0, 0);
+}
