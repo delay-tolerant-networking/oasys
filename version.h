d772c35194e20fa213a30057d52af7a1a3b69b9b
demmer <devnull@localhost>
2005-03-08 18:55:25 -0800

    bump version number

--- version.h
+++ version.h
@@ -2,9 +2,9 @@
 #define _OASYS_VERSION_H_
 
 #define OASYS_VERSION_MAJOR	1
-#define OASYS_VERSION_MINOR	0
+#define OASYS_VERSION_MINOR	1
 #define OASYS_VERSION_PATCH	0
-#define OASYS_VERSION_STRING	"OASYS Library version 1.0.0"
+#define OASYS_VERSION_STRING	"OASYS Library version 1.1.0"
 
 extern const char* oasys_version;
 
