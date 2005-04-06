b9b5eebeeb8e59b0d32708a6396ed8fb76e52a5f
bowei <devnull@localhost>
2005-04-05 20:06:00 -0700

    * adding ScopePtr * adding Singleton

---
+++ util/Pointers.h
@@ -0,0 +1,42 @@
+#ifndef __POINTERS_H__
+#define __POINTERS_H__
+
+namespace oasys {
+
+/**
+ * NOTE: Used only for things that are created which need to be
+ * deleted when exiting a scope. This is mainly used to clean up call
+ * site regions where the functions allocate memory and take a double
+ * pointer as an argument, which makes it impossible to use a
+ * std::auto_ptr.
+ */
+template <typename _Class>
+class ScopePtr {
+public:
+    ScopePtr() : ptr_(0) {}
+    ScopePtr(_Class* obj) : ptr_(obj) {}
+    ~ScopePtr() { if(ptr_) { delete ptr_; ptr_ = 0; } }
+
+    _Class& operator*()  const { return *ptr_; }
+    _Class* operator->() const { return ptr_; }
+
+    /**
+     * This construction basically allows you to pass the ptr_ to a
+     * double pointer taking function, cleaning up the syntax quite a
+     * bit.
+     */
+    _Class*& get() { return ptr_; }
+
+    /**
+     * Not implemented on purpose. Don't handle assignment to another
+     * ScopePtr
+     */
+    ScopePtr& operator=(const ScopePtr&); 
+    
+private:
+    _Class* ptr_;
+};
+
+} // namespace oasys
+
+#endif //__POINTERS_H__
