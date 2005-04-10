bb007001d204d222a06f904d47294e7f221fe094
bowei <devnull@localhost>
2005-04-10 16:00:36 -0700

    * fixed bogus pointer bug

--- test/berkeley-db-test.cc
+++ test/berkeley-db-test.cc
@@ -15,20 +15,20 @@ const char*    g_err_log_name = "output/berkeley-db-test/err.log";
 DurableTableId g_id;
 
 DECLARE_TEST(DBInit) {
-    BerkeleyStore::init_for_debug(g_db_name,
-                                  g_config_dir,
-                                  g_err_log_name,
-                                  true, 0);
+    BerkeleyStore::init(g_db_name,
+                        g_config_dir,
+                        g_err_log_name,
+                        true, 0);
 
     BerkeleyStore::shutdown();
     return 0;
 }
 
 DECLARE_TEST(TableCreate) {
-    BerkeleyStore::init_for_debug(g_db_name,
-                                  g_config_dir,
-                                  g_err_log_name,
-                                  false, 0);
+    BerkeleyStore::init(g_db_name,
+                        g_config_dir,
+                        g_err_log_name,
+                        false, 0);
 
     DurableTable* table1;
     DurableTable* table2;
@@ -56,10 +56,10 @@ DECLARE_TEST(TableCreate) {
 }
 
 DECLARE_TEST(Insert) {
-    BerkeleyStore::init_for_debug(g_db_name,
-                                  g_config_dir,
-                                  g_err_log_name,
-                                  false, 0);
+    BerkeleyStore::init(g_db_name,
+                        g_config_dir,
+                        g_err_log_name,
+                        false, 0);
     DurableTableStore* store = DurableTableStore::instance();
     DurableTable* table;
     CHECK(store->new_table(&table) == 0);
@@ -78,13 +78,13 @@ DECLARE_TEST(Insert) {
     }
 
     delete table; table = 0;
-
     BerkeleyStore::shutdown();
 
-    BerkeleyStore::init_for_debug(g_db_name,
-                                  g_config_dir,
-                                  g_err_log_name,
-                                  false, 0);
+    BerkeleyStore::init(g_db_name,
+                        g_config_dir,
+                        g_err_log_name,
+                        false, 0);
+    store = DurableTableStore::instance();
     CHECK(store->get_table(g_id, &table) == 0);
 
     PermutationArray pa(500);
@@ -108,10 +108,10 @@ DECLARE_TEST(Insert) {
 }
 
 DECLARE_TEST(Delete) {
-    BerkeleyStore::init_for_debug(g_db_name,
-                                  g_config_dir,
-                                  g_err_log_name,
-                                  false, 0);
+    BerkeleyStore::init(g_db_name,
+                        g_config_dir,
+                        g_err_log_name,
+                        false, 0);
     DurableTableStore* store = DurableTableStore::instance();
     DurableTable* table;
     
@@ -142,10 +142,10 @@ DECLARE_TEST(Delete) {
 }
 
 DECLARE_TEST(Iterator) {
-    BerkeleyStore::init_for_debug(g_db_name,
-                                  g_config_dir,
-                                  g_err_log_name,
-                                  false, 0);
+    BerkeleyStore::init(g_db_name,
+                        g_config_dir,
+                        g_err_log_name,
+                        false, 0);
     DurableTableStore* store = DurableTableStore::instance();
     DurableTable* table;
      
