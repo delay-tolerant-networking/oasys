b6332077355b2e23668ed6fa9903d55087ef1286
demmer <devnull@localhost>
2005-09-22 12:13:34 -0700

    add import_find utility to search the path for files

--- test-utils/import.tcl
+++ test-utils/import.tcl
@@ -2,19 +2,14 @@
 # Tcl doesn't have a useful import mechanism so we just do a simple one
 #
 
-proc import {script} {
+proc import_find {script} {
     global import_path
-
-    if {$script == ""} {
-	error "must specify a script to import"
-    }
 
     # check for full paths
     if [file readable $script] {
-	source $script
-	return
-    } 
-
+	return $script
+    }
+    
     # otherwise we need the import_path
     if {![info exists import_path]} {
 	error "cannot import without setting import_path"
@@ -23,10 +18,19 @@ proc import {script} {
     foreach dir $import_path {
 	set path [file join $dir $script]
 	if {[file readable $path]} {
-	    source $path
-	    return
+	    return $path
 	}
     }
-
+    
     error "can't find script $script in import_path $import_path"
 }
+    
+proc import {script} {
+    if {$script == ""} {
+	error "must specify a script to import"
+    }
+    
+    source [import_find $script]
+}
+
+
