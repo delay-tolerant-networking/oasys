0d8d56ae38db5b022fb6d354a66fdc9a13486557
demmer <devnull@localhost>
2005-10-05 18:17:37 -0700

    add little shell script to run the unit tests

---
+++ test/run-unit-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+./berkeley-db-test
+# ./buffered-io-test
+./io-basic-test
+./io-core-test
+./marshal-test
+./optparser-test
+./serialize-test
+./static-buffer-test
+./string-hash-test
+./timer-test
+./type-collection-test
