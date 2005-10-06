e178f0455209a38f7404b1b98bac68cd33e7432e
bowei <devnull@localhost>
2005-10-06 11:45:37 -0700

    * add run only unit tests

--- test/run-unit-tests.sh
+++ test/run-unit-tests.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-./berkeley-db-test
-# ./buffered-io-test
-./io-basic-test
-./io-core-test
-./marshal-test
-./optparser-test
-./serialize-test
-./static-buffer-test
-./string-hash-test
-./timer-test
-./type-collection-test
+for i in *; do
+    if [ \( -d $i \) -o \( $i = "UnitTest.tcl" \) -o \( $i = "run-unit-tests.sh" \) ]; then
+	continue
+    fi
+
+    if [ -x $i ]; then
+	is_unit_test=`grep "UnitTest\\.h" $i.cc`
+	if [ ! -z "$is_unit_test" ]; then
+	    echo "* Running test $i"
+	    ./$i
+	fi
+    fi
+done
