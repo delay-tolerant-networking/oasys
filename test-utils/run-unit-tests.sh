5cfdbfff38271cfbefc90bf52833d7b793e60208
podolsky <devnull@localhost>
2005-10-06 17:46:09 -0700

    * print a warning if no executables found and run

--- test-utils/run-unit-tests.sh
+++ test-utils/run-unit-tests.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+found_tests=0
 for i in *; do
     if [ -f $i -a -x $i ]; then
+        found_tests=1
         echo "***"
 	echo "*** $i"
 	echo "***"
         ./$i
     fi
 done
+
+if [ $found_tests = 0 ]; then
+    echo "warning: no tests found"
+fi
+
