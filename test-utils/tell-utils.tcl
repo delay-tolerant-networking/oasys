2c319bda51f82535e3bf49ec5c1a4eec3406ea99
demmer <devnull@localhost>
2005-10-21 10:02:36 -0700

    more robustness added to try to debug problems on cygwin

--- test-utils/tell-utils.tcl
+++ test-utils/tell-utils.tcl
@@ -25,8 +25,17 @@ namespace eval tell {
 	if {$tell_verbose} {
 	    puts "tell:connect trying to connect to $host:$port"
 	}
-	
-	set sock [socket $host $port]
+
+	set sock "none"
+	if [catch {
+	    set sock [socket $host $port]
+	} err] {
+	    global errorInfo
+	    if {$tell_verbose} {
+		puts "tell::connect error connecting to $host:$port (sock $sock)"
+	    }
+	    error $err $errorInfo
+	}
 
 	puts $sock "tell_encode"
 	flush $sock
@@ -37,8 +46,8 @@ namespace eval tell {
 	}
 
 	fconfigure $sock -blocking 0
-	fileevent $sock readable "tell::response $host $port"
-	set tell::sockets($host:$port) $sock
+	fileevent $sock readable "tell::response $sock $host $port"
+	set sockets($host:$port) $sock
 
 	if {$tell_verbose} {
 	    puts "tell::connect success to $host:$port (sock $sock)"
@@ -46,13 +55,21 @@ namespace eval tell {
     }
 
     proc wait {host port {timeout 30000}} {
-	global tell::sockets
+	global tell::sockets tell::tell_verbose
+	
 	if {[info exists sockets($host:$port)]} {
 	    return
+	}
+
+	if {$tell_verbose} {
+	    puts "tell::wait: no socket to $host:$port, connecting"
 	}
 	
 	do_until "connecting to $host:$port" $timeout {
 	    if {![catch {tell::connect $host $port} err]} {
+		if {$tell_verbose} {
+		    puts "tell::wait: connected to $host:$port (sock $sockets($host:$port))"
+		}
 		return
 	    }
 	    after 1000
@@ -63,6 +80,9 @@ namespace eval tell {
 	global tell::sockets tell::results tell::tell_verbose tell::timeout
 
 	if {![info exists sockets($host:$port)]} {
+	    if {$tell_verbose} {
+		puts "tell::tell: no socket to $host:$port, connecting"
+	    }
 	    tell::connect $host $port
 	}
 	set sock $sockets($host:$port)
@@ -74,7 +94,7 @@ namespace eval tell {
 	}
 	
 	if {$tell_verbose} {
-	    puts "tell::tell: sending command \"$cmd\""
+	    puts "tell::tell: sending command \"$cmd\" to $host:$port"
 	}
 	
 	regsub -all -- {\n} [string trim $cmd] {\\n} cmd
@@ -82,19 +102,20 @@ namespace eval tell {
 	flush $sock
 
 	# set up a timeout in case there's no response
-	set timer [after $tell::timeout "tell::timeout $host $port"]
+	set timer [after $tell::timeout "tell::timeout $sock $host $port"]
 	
 	# wait for a response
 	set tell::results($host:$port) ""
 	while {$tell::results($host:$port) == ""} {
 	    vwait tell::results($host:$port)
+	    puts "tell::tell returned from vwait $host:$port"
 	}
 
 	# cancel the timer (if it exists)
-	catch {after cancel $timer}
+	catch {after cancel $timer} err
 	
 	if {$tell_verbose} {
-	    puts "tell::tell: got result $tell::results($host:$port)"
+	    puts "tell::tell: got result '$tell::results($host:$port)' from $host:$port"
 	}
 	
 	set cmd_error [string index $tell::results($host:$port) 0]
@@ -113,6 +134,7 @@ namespace eval tell {
 	    if {$result == ""} {
 		set result "(no additional information)"
 	    }
+	    puts "tell error '$result' '$errorInfo'"
 	    error $result $errorInfo
 	}
     }
@@ -126,27 +148,34 @@ namespace eval tell {
 	
 	catch {
 	    fileevent $tell::sockets($host:$port) readable ""
+	}
+
+	catch {
 	    close $tell::sockets($host:$port)
 	}
 	
 	unset tell::sockets($host:$port)
     }
     
-    proc response {host port} {
+    proc response {sock host port} {
 	global tell::sockets tell::results tell::tell_verbose
-	set sock $tell::sockets($host:$port)
 
-	set cmd_response [gets $sock]
-
-	if {$cmd_response == "" && [eof $sock]} {
-	    puts "error: eof in tell command to $host:$port"
-
-	    tell::close_socket $host $port
-	    set tell::results($host:$port) \
-		    "1 error: eof while waiting for tell response\\n"
+	if {![info exists sockets($host:$port)] ||\
+		$sock != $sockets($host:$port)} {
+	    puts "XXX WARNING: bogus fileevent for stale socket $sock ($host:$port)"
 	    return
 	}
 
+	set cmd_response [gets $sock]
+
+	if {($cmd_response == "") && [eof $sock]} {
+	    puts "error: eof in tell command to $host:$port"
+
+	    tell::close_socket $host $port
+	    set results($host:$port) "1 error: eof while waiting for tell response"
+	    return
+	}
+	
 	if {$cmd_response == ""} {
 	    # short read, means there's not a full line but we should
 	    # get another callback when more data arrives
@@ -155,19 +184,24 @@ namespace eval tell {
 	}
 
 	if {$tell_verbose} {
-	    puts "tell:response $host:$port $cmd_response"
+	    puts "tell::response $host:$port '$cmd_response'"
 	}
 	
-	set tell::results($host:$port) $cmd_response
+	set results($host:$port) $cmd_response
     }
 
-    proc timeout {host port} {
-	global tell::results tell::tell_verbose
+    proc timeout {sock host port} {
+	global tell::sockets tell::results tell::tell_verbose
 
-	puts "error: timeout in tell command to $host:$port"
+	if {![info exists sockets($host:$port)] ||\
+		$sock != $sockets($host:$port)} {
+	    puts "XXX WARNING: bogus timeout for stale socket $sock ($host:$port)"
+	    return
+	}
+	
+	puts "error: timeout in tell command to $host:$port ($sock)"
 	
 	tell::close_socket $host $port
-	set tell::results($host:$port) \
-		"1 error: timeout while waiting for tell response\\n"
+	set tell::results($host:$port) "1 error: timeout while waiting for tell response"
     }
 }
