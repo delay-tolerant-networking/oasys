928c62d3de99b5d7f5fff671a52ce96f5f0c9cbe
demmer <devnull@localhost>
2005-10-26 16:34:09 -0700

    change the script to wait a maximum of five minutes for any one test to complete before aborting it

--- test-utils/run-unit-tests.sh
+++ test-utils/run-unit-tests.sh
@@ -1,14 +1,36 @@
 #!/bin/sh
 
+run_and_wait() {
+    prog=$1
+
+    echo "***"
+    echo "*** $prog"
+    echo "***"
+
+    $prog &
+    pid=$!
+
+    timeout=300
+    while [ 1 ]; do
+       ps -h $pid > /dev/null 2>&1
+       [ $? = 1 ] && break
+       sleep 1
+
+       timeout=$((timeout - 1))
+       if [ $timeout == 0 ]; then
+	   echo "error: unit test took too long!!!"
+	   kill -ABRT $pid
+	   break
+       fi
+    done
+}
+
 (
 found_tests=0
 for i in *; do
     if [ -f $i -a -x $i ]; then
         found_tests=1
-        echo "***"
-	echo "*** $i"
-	echo "***"
-        ./$i
+        run_and_wait $i 0
     fi
 done
 
