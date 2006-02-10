176e4129070367c9ffa9a58f1620f100dd5fa6e3
bowei <devnull@localhost>
2006-02-09 16:21:31 -0800

    * helper functions which reduce the amount of code needed for   writing serialization routines

--- serialize/SerializeHelpers.h
+++ serialize/SerializeHelpers.h
@@ -3,9 +3,12 @@
 
 #include <vector>
 
+#include "Serialize.h"
+
 namespace oasys {
 
-template<typename _type> int
+//----------------------------------------------------------------------------
+template<typename _type> void
 serialize_basic_vector(oasys::SerializeAction* action,
                        std::string             name,
                        std::vector<_type>*     v)
@@ -13,15 +16,22 @@ serialize_basic_vector(oasys::SerializeAction* action,
     name += ".size";
 
     size_t size;
-    size = v->size()
+    size = v->size();
     action->process(name.c_str(), &size);
+    
+    if (action->error())
+        return;
+
     v->resize(size);
     
     for (size_t i=0; i<size; ++i)
     {
         action->process(name.c_str(), &((*v)[i]));
+        if (action->error())
+            return;
     }
 }
 
 } // namespace oasys
+
 #endif /* __SERIALIZEHELPERS_H__ */
