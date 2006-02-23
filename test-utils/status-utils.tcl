c670efbab60ec54646f37f044e56d5515733f772
bowei <devnull@localhost>
2006-02-22 19:23:05 -0800

    * add xterm command (flaky for some reason)

--- test-utils/status-utils.tcl
+++ test-utils/status-utils.tcl
@@ -3,7 +3,7 @@
 #
 
 proc help {} {
-    puts "commands: help status"
+    puts "commands: help status xterm"
 }
 
 proc status {} {
@@ -17,6 +17,20 @@ proc status {} {
 	    set status "dead"
 	}
 	puts "host $i $hostname $dir $status"
+    }
+}
+
+proc xterm {args} {
+    for {set i 0} {$i < $::net::nodes} {incr i} {
+	set dir $::dist::distdirs($i)
+	set hostname $::net::host($i)
+	
+	# ssh will barf trying to lock the xauthority XXX/bowei --
+	# this still has some mysterious problems and interactions
+	# with the command prompt
+	after 100
+	
+	eval "exec ssh -X $hostname $args \"cd $dir; xterm\" &"
     }
 }
 
