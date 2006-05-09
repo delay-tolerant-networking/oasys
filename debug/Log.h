e8719f0724317db5044652b6473ddffefbce750d
demmer <devnull@localhost>
2006-05-09 14:46:48 -0700

    * when NDEBUG is defined, turn the log_debug macro into '0', not into   nothing so code that checks the return value will still compile

--- debug/Log.h
+++ debug/Log.h
@@ -512,8 +512,8 @@ __log_enabled(log_level_t level, const char* path)
 
 // compile out all log_debug calls when not debugging
 #ifdef NDEBUG
-#define log_debug(args...)
-#define __log_debug(args...)
+#define log_debug(args...)   0
+#define __log_debug(args...) 0
 #else
 #define log_debug(p, args...)                                   \
     ((__log_enabled(oasys::LOG_DEBUG, (p))) ?                   \
