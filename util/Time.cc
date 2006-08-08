79422a63f1dad0c075177f3e82be65189764f67c
demmer <devnull@localhost>
2006-08-07 17:53:09 -0700

    * pull in updated Time.cc from the win32 port branch

--- util/Time.cc
+++ util/Time.cc
@@ -1,11 +1,14 @@
 #include "Time.h"
 
-#ifndef __win32__
+#if defined(_WIN32)
+
+#include "compat/MSVC.h"
+
+#else
+
 #include <sys/time.h>
 #include <time.h>
-#else
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+
 #endif
 
 namespace oasys {
@@ -14,15 +17,7 @@ namespace oasys {
 void
 Time::get_time() 
 {
-#ifndef __win32__
-
-    struct timeval tv;
-
-    gettimeofday(&tv, 0);
-    sec_  = (unsigned int) tv.tv_sec;
-    usec_ = (unsigned int) tv.tv_usec;
-
-#else
+#if defined(_WIN32)
 
     SYSTEMTIME systime;
     FILETIME   filetime;
@@ -41,11 +36,18 @@ Time::get_time()
 
     // Get microseconds
     ft /= 10; 
-    usec_ = ft % 1000000;
+    usec_ = static_cast<u_int32_t>(ft % 1000000);
 
     // Get seconds
     ft /= 1000000;
-    sec_ = ft;
+    sec_ = static_cast<u_int32_t>(ft);
+
+#else
+    struct timeval tv;
+
+    gettimeofday(&tv, 0);
+    sec_  = static_cast<u_int32_t>(tv.tv_sec);
+    usec_ = static_cast<u_int32_t>(tv.tv_usec);
 
 #endif
 
@@ -53,17 +55,85 @@ Time::get_time()
 }
 
 //----------------------------------------------------------------------------
+double 
+Time::in_seconds()
+{
+    return static_cast<double>(sec_) + 
+        static_cast<double>(usec_)/1000000;
+}
+
+//----------------------------------------------------------------------------
+u_int32_t
+Time::in_microseconds()
+{
+    return sec_ * 1000000 + usec_;
+}
+    
+//----------------------------------------------------------------------------
+u_int32_t
+Time::in_milliseconds()
+{
+    return sec_ * 1000 + usec_/1000;
+}
+
+//----------------------------------------------------------------------------
 Time
-Time::operator+(const Time& t)
+Time::operator+(const Time& t) const
 {
     return Time(t.sec_ + sec_, t.usec_ + usec_);
 }
 
 //----------------------------------------------------------------------------
 Time
-Time::operator-(const Time& t)
+Time::operator-(const Time& t) const
 {
-    return Time(sec_ - t.sec_, usec_ - t.usec_);
+    // a precondition for this fn to be correct is (*this >= t)
+    Time t2(*this);
+    if (t2.usec_ < t.usec_) {
+        t2.usec_ += 1000000;
+        t2.sec_  -= 1;
+    }
+
+    t2.sec_  -= t.sec_;
+    t2.usec_ -= t.usec_;
+    return t2;
+}
+
+//----------------------------------------------------------------------------
+bool
+Time::operator==(const Time& t) const
+{
+    return (sec_ == t.sec_) && (usec_ == t.usec_);
+}
+
+//----------------------------------------------------------------------------
+bool
+Time::operator<(const Time& t) const
+{
+    return (sec_ < t.sec_) 
+        || ( (sec_ == t.sec_) && (usec_ < t.usec_));
+}
+
+//----------------------------------------------------------------------------
+bool
+Time::operator>(const Time& t) const
+{
+    return (sec_ > t.sec_) 
+        || ( (sec_ == t.sec_) && (usec_ > t.usec_));    
+}
+
+//----------------------------------------------------------------------------
+bool
+Time::operator<=(const Time& t) const
+{
+    return (*this == t) || (*this < t);
+}
+
+//----------------------------------------------------------------------------
+bool
+Time::operator>=(const Time& t) const
+{
+    return (*this == t) || (*this > t);
 }
  
 //----------------------------------------------------------------------------
