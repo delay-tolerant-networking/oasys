ad7e0939a56c101bdb5b4ba8a62bbfce15841f10
demmer <devnull@localhost>
2006-09-19 13:40:37 -0700

    From Jeff Wilson: * Reduce and streamline inquiry: -- no longer reads remote name of discovered device, upon enumeration -- enumeration returns only remote adapter address -- esoteric mutators now hardcoded to best practice defaults * Subsequently obsoletes BluetoothInquiryInfo utility class

--- bluez/BluetoothInquiry.cc
+++ bluez/BluetoothInquiry.cc
@@ -1,4 +1,3 @@
-
 #include <config.h>
 #ifdef OASYS_BLUETOOTH_ENABLED
 
@@ -12,6 +11,7 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/rfcomm.h>
 
+#include "io/IO.h"
 #include "../debug/Log.h"
 #include "Bluetooth.h"
 #include "BluetoothInquiry.h"
@@ -20,194 +20,59 @@ namespace oasys {
 
 BluetoothInquiry::BluetoothInquiry(const char * logbase)
     : Logger("BluetoothInquiry", logbase),
-      hci_dev_(0),
-      num_responses_(10),
       num_responses_i_(-1),
-      length_(8),
-      lap_(NULL),
-      info_(NULL),
-      flags_(0L),
-      timeout_(100000),
-      fd_(-1),
-      pos_(0)
+      pos_(0),
+      flags_(0L)
 {
-    hci_device_name_ = NULL;
-    set_hci_device_name("hci0");
+    memset(&info_[0],0,sizeof(inquiry_info)*BT_INQ_NUM_RESP);
 }
 
 BluetoothInquiry::~BluetoothInquiry()
 {
-    if(fd_ != -1)
-    {
-close(fd_);
-    }
-
-    if(info_)
-    {
-        free(info_);
-    }
-    delete [] hci_device_name_;
 }
 
-char *
-BluetoothInquiry::hci_device_name()
-{
-    return hci_device_name_;
-}
-
-void
-BluetoothInquiry::set_hci_device_name(char *hci_device_name )
-{
-    int sz = strlen(hci_device_name) + 1;
-    if (hci_device_name_) {
-        delete [] hci_device_name_;
-    }
-    hci_device_name_ = new char[sz];
-    strncpy(hci_device_name_, hci_device_name, sz);
-    hci_dev_ = Bluetooth::hci_devid(hci_device_name_,logpath_);
-}
-
-int
-BluetoothInquiry::num_responses()
-{
-    return num_responses_;
-}
-
-void
-BluetoothInquiry::set_numresponses(int nr)
-{
-    ASSERT( nr > 0 && nr < 250 );
-    num_responses_ = nr;
-}
-
-int
-BluetoothInquiry::length()
-{
-    return length_;
-}
-
-void
-BluetoothInquiry::set_length(int length)
-{
-    ASSERT(length > 0 && length < 20);
-    length_ = length;
-}
-
-uint8_t *
-BluetoothInquiry::lap()
-{
-    return lap_;
-}
-
-void
-BluetoothInquiry::set_lap(uint8_t *lap )
-{
-    lap_ = lap;
-}
-
-inquiry_info *
-BluetoothInquiry::info()
-{
-    return info_;
-}
-
-void
-BluetoothInquiry::set_info(inquiry_info *info)
-{
-    info_ = info;
-}
-
-long
-BluetoothInquiry::flags()
-{
-    return flags_;
-}
-
-void
-BluetoothInquiry::set_flags(long flags)
-{
-    flags_ = flags;
-}
- 
 int
 BluetoothInquiry::inquire()
 {
-    if(info_)
-    {
-        free(info_);
-        info_ = NULL;
-    }
-    num_responses_i_ = Bluetooth::hci_inquiry(
-                           hci_dev_,length_,num_responses_,
-                           lap_,&info_,flags_,logpath_);
-    if( num_responses_i_ < 0 )
-    {
-        log_info("hci_inquiry found no devices in range");
-    }
+    // set dev_id to -1 to have BlueZ autodetermine which device
+    // set len = 8 for default 10.24s duration
+    // set nrsp = BT_INQ_NUM_RESP to match sizeof(info_)
+    // Lower Address Part (lap) is ignored (0)
+    // info_ is return array of inquiry data
+    // flags_ changes the default inquiry behavior
+    inquiry_info *ii = &info_[0];
+    num_responses_i_ = Bluetooth::hci_inquiry(-1,BT_INQ_LENGTH,BT_INQ_NUM_RESP,
+                                              0,&ii,flags_);
     return num_responses_i_;
-}
-
-int
-BluetoothInquiry::timeout()
-{
-    return timeout_;
-}
- 
-void
-BluetoothInquiry::set_timeout(int to)
-{
-    timeout_ = to;
 }
 
 void
 BluetoothInquiry::reset()
 {
     pos_ = 0;
-    if( fd_ != -1 ) {
-        close(fd_);
-        fd_ = -1;
-    }
+    num_responses_i_ = -1;
+    memset(&info_[0],0,sizeof(inquiry_info)*BT_INQ_NUM_RESP);
     flags_ |= IREQ_CACHE_FLUSH;
 }
 
 int
-BluetoothInquiry::first(BluetoothInquiryInfo& bii)
+BluetoothInquiry::first(bdaddr_t& addr)
 {
     reset();
-    return next(bii);
+    return next(addr);
 }
 
 int
-BluetoothInquiry::next(BluetoothInquiryInfo& bii)
+BluetoothInquiry::next(bdaddr_t& addr)
 {
-    char buff[18];
-    (void)buff;
-
-    ASSERT( info_ != NULL );
-    if(pos_ >= num_responses_i_)
+    if((pos_ >= num_responses_i_) || 
+       (bacmp(&(info_[pos_].bdaddr),BDADDR_ANY) == 0))
     {
+        reset();
         return -1;
     }
     log_debug("BluetoothInquiry::next(%d)",pos_);
-    if(fd_ == -1)
-    {
-        fd_ = Bluetooth::hci_open_dev(hci_dev_,logpath_);
-        if(fd_ == -1)
-        {
-            log_err("hci_open_dev failed");
-            return -1;
-        }
-    }
-    memset(bii.name_,0,bii.name_len_);
-    if(Bluetooth::hci_read_remote_name(fd_,&(info_ + pos_)->bdaddr,
-                                   bii.name_len_,bii.name_,
-                                   timeout_) < 0 )
-    {
-        strcpy(bii.name_,"[none]");
-    }
-    bacpy(&bii.addr_,&(info_ + pos_)->bdaddr); 
-    log_debug("read remote name %s(%s)",bii.name_,
-              Bluetooth::batostr(&bii.addr_,buff));
+    bacpy(&addr,&(info_[pos_].bdaddr));
     pos_++;
     return 0;
 }
--- bluez/BluetoothInquiry.h
+++ bluez/BluetoothInquiry.h
@@ -10,101 +10,44 @@
 
 #include "../debug/Log.h"
 
+#define MAX_BTNAME_SZ 248
+
+#ifndef MIN
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#endif
+
 namespace oasys {
-
-class BluetoothInquiryInfo {
-public:
-    BluetoothInquiryInfo(char* name="", int name_len=248) 
-        : name_(name),
-          name_len_(name_len)
-    {
-        memset(&addr_,0,sizeof(addr_));
-        name_ = new char[name_len_];
-        memset(name_,0,name_len_);
-    }
-
-    BluetoothInquiryInfo(const BluetoothInquiryInfo& btc)
-    { 
-        memset(&addr_,0,sizeof(addr_));
-        bacpy(&this->addr_,&btc.addr_);
-
-        name_len_ = btc.name_len_;
-        name_ = new char[name_len_];
-        memset(name_,0,name_len_);
-        strncpy(name_,btc.name_,name_len_);
-    }
-
-    ~BluetoothInquiryInfo()
-    {
-        delete [] name_;
-    }
-
-    char *name_;
-    int name_len_;
-    bdaddr_t addr_;
-};
 
 class BluetoothInquiry : public Logger {
 
 public:
-    BluetoothInquiry(const char * logbase = "/btinquiry");
+
+#define BT_INQ_NUM_RESP 20
+#define BT_INQ_LENGTH 8
+
+    BluetoothInquiry(const char * logbase = "/dtn/btinquiry");
     ~BluetoothInquiry();
 
-    /*
-     * Set/get inquiry parameters
-     */
-    char * hci_device_name();
-    void set_hci_device_name(char*);
-
-    /// number of desired responses to wait for
-    int num_responses();
-    void set_numresponses(int);
-
-    /// number of 1.28s intervals to scan
-    int length();
-    void set_length(int);
-
-    /// lower address part ... see p. 64, vol 1, part B of Bluetooth 2.0
-    uint8_t* lap();
-    void set_lap(uint8_t*);
-
-    inquiry_info* info();
-    void set_info(inquiry_info*);
-    
-    long flags();
-    void set_flags(long);
-    
     /*
      * Perform inquiry action
      */
     int inquire();
 
     /*
-     * Set/get read_remote_name parameters
+     * Enumerate over discovered device adapter addresses
      */
-    int timeout();
-    void set_timeout(int);
-
-    /*
-     * Perform read_remote_name action
-     */
-    int first(BluetoothInquiryInfo& bti);
-    int next(BluetoothInquiryInfo& bti);
+    int first(bdaddr_t&);
+    int next(bdaddr_t&);
 
 protected:
     void reset();  // performed internally between inquiries
 
-    char *hci_device_name_;
-    int hci_dev_;
-    int num_responses_,num_responses_i_;
-    int length_;
-    uint8_t *lap_;
-    inquiry_info *info_;
+    // state and iterator over info_
+    int num_responses_i_, pos_;
+    // buffer for query data
+    inquiry_info info_[BT_INQ_NUM_RESP];
+    // flags to change default inquiry behavior
     long flags_;
-    int timeout_;
-
-    int fd_;
-    int pos_;
 };
 
 }
