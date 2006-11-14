cda1364cf82365e868ac8619ff0860355a46c2cc
demmer <devnull@localhost>
2006-11-13 16:20:40 -0800

    * add unit tests for copy constructors in scratch buffers

--- test/buffer-test.cc
+++ test/buffer-test.cc
@@ -93,6 +93,83 @@ DECLARE_TEST(ScratchBuffer1) {
     return UNIT_TEST_PASSED;
 }
 
+DECLARE_TEST(ScratchBufferCopy1) {
+    const char* str = "0987654321";
+    ScratchBuffer<char*, 20> scratch;
+    memcpy(scratch.buf(), str, 10);
+    scratch.set_len(10);
+    CHECK_EQUALSTRN(scratch.buf(), str, 10);
+
+    ScratchBuffer<char*, 20> scratch2(scratch);
+    CHECK_EQUALSTRN(scratch2.buf(), str, 10);
+    CHECK_EQUAL(scratch.len(), scratch2.len());
+    CHECK_EQUAL(scratch.buf_len(), scratch2.buf_len());
+
+    CHECK(scratch.buf() != scratch2.buf());
+
+    return UNIT_TEST_PASSED;
+}
+
+DECLARE_TEST(ScratchBufferCopy2) {
+    const char* str = "0987654321";
+    ScratchBuffer<char*, 20> scratch;
+    
+    scratch.reserve(10);
+    memcpy(scratch.buf(), str, 10);
+    
+    scratch.reserve(20);
+    memcpy(scratch.buf()+10, str, 10);
+    
+    scratch.reserve(30);
+    memcpy(scratch.buf()+20, str, 10);
+    
+    scratch.set_len(30);
+    
+    CHECK_EQUALSTRN(scratch.buf(), str, 10);
+    CHECK_EQUALSTRN(scratch.buf()+10, str, 10);
+    CHECK_EQUALSTRN(scratch.buf()+20, str, 10);
+
+    ScratchBuffer<char*, 20> scratch2(scratch);
+    CHECK_EQUALSTRN(scratch.buf(), scratch2.buf(), 30);
+    CHECK_EQUAL(scratch.len(), scratch2.len());
+    CHECK_EQUAL(scratch.buf_len(), scratch2.buf_len());
+
+    CHECK(scratch.buf() != scratch2.buf());
+
+    return UNIT_TEST_PASSED;
+}
+
+struct Foo {
+    Foo(int i) : idx(i) {}
+    
+    int idx;
+    ScratchBuffer<char*, 20> scratch;
+};
+
+DECLARE_TEST(ScratchBufferCopy3) {
+    const char* str = "0987654321";
+
+    std::vector<Foo> vec;
+    for (int i = 0; i < 10; ++i) {
+        vec.push_back(Foo(i));
+        Foo* f = &vec.back();
+        
+        f->scratch.reserve(30);
+        f->scratch.set_len(30);
+        memcpy(f->scratch.buf(), str, 10);
+        memcpy(f->scratch.buf()+10, str, 10);
+        memcpy(f->scratch.buf()+20, str, 10);
+    }
+
+    for (int i = 0; i < 9; ++i) {
+        for (int j = i + 1; j < 10; ++j) {
+            CHECK(vec[i].scratch.buf() != vec[j].scratch.buf());
+        }
+    }
+    
+    return UNIT_TEST_PASSED;
+}
+
 DECLARE_TEST(StringBuffer1) {
     StringBuffer buf(256);
     char scratch[1024];
@@ -154,6 +231,9 @@ DECLARE_TESTER(Test) {
     ADD_TEST(ExpandableBuffer1);
     ADD_TEST(ExpandableBuffer2);
     ADD_TEST(ScratchBuffer1);
+    ADD_TEST(ScratchBufferCopy1);
+    ADD_TEST(ScratchBufferCopy2);
+    ADD_TEST(ScratchBufferCopy3);
     ADD_TEST(StringBuffer1);
     ADD_TEST(StringBuffer2);
     ADD_TEST(StringBuffer3);
