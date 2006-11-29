4707e2a1b5f83c3236e3a1a99c5aedc3fd7c80f0
bowei <devnull@localhost>
2006-11-29 13:17:56 -0800

    * tested string appender class

--- util/StringAppender.cc
+++ util/StringAppender.cc
@@ -4,13 +4,13 @@
 namespace oasys {
 
 //----------------------------------------------------------------------------
-StringAppener::StringAppender(char* buf, size_t size)
+StringAppender::StringAppender(char* buf, size_t size)
     : cur_(buf), remaining_(size), len_(0)
 {}
 
 //----------------------------------------------------------------------------
 size_t 
-StringAppener::append(const char* str, size_t len = 0)
+StringAppender::append(const char* str, size_t len)
 {
     if (remaining_ == 0) 
     {
@@ -28,7 +28,7 @@ StringAppener::append(const char* str, size_t len = 0)
     
     cur_       += len;
     remaining_ -= len;
-    size_      += len;
+    len_       += len;
 
     ASSERT(*cur_ == '\0');
 
@@ -37,7 +37,7 @@ StringAppener::append(const char* str, size_t len = 0)
 
 //----------------------------------------------------------------------------
 size_t 
-StringAppener::append(char c)
+StringAppender::append(char c)
 {
     if (remaining_ <= 1) 
     {
@@ -47,7 +47,7 @@ StringAppener::append(char c)
     *cur_ = c;
     ++cur_;
     --remaining_;
-    ++size_;
+    ++len_;
     *cur_ = '\0';
 
     ASSERT(*cur_ == '\0');
@@ -57,10 +57,10 @@ StringAppener::append(char c)
 
 //----------------------------------------------------------------------------
 size_t 
-StringAppener::appendf(const char* fmt, ...)
+StringAppender::appendf(const char* fmt, ...)
 {
     va_list ap;
-    va_start(ap);
+    va_start(ap, fmt);
     size_t ret = vappendf(fmt, ap);
     va_end(ap);
 
@@ -69,7 +69,7 @@ StringAppener::appendf(const char* fmt, ...)
 
 //----------------------------------------------------------------------------
 size_t 
-StringAppener::vappendf(const char* fmt, va_list ap)
+StringAppender::vappendf(const char* fmt, va_list ap)
 {
     if (remaining_ == 0) 
     {
@@ -79,9 +79,9 @@ StringAppener::vappendf(const char* fmt, va_list ap)
     size_t ret = vsnprintf(cur_, remaining_, fmt, ap);
     ret = std::min(ret, remaining_ - 1);
 
-    cur       += ret;
-    remaining -= ret;
-    size      += ret;
+    cur_       += ret;
+    remaining_ -= ret;
+    len_       += ret;
 
     ASSERT(*cur_ == '\0');
 
