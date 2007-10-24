2eb8cee4af503bc51c4e9cab42151ecc399b3941
Michael Demmer <demmer@cs.berkeley.edu>
2007-10-23 19:34:21 -0700

    run make install

--- install.sh
+++ install.sh
@@ -14,10 +14,4 @@
 #    limitations under the License.
 #
 
-#
-# autoconf complains if we don't have an install script, and I should
-# write one some time anyway...
-#
-
-echo "XXX install todo"
-exit 1
+make install
